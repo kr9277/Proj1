b782f04d0ef14222be0db7a90b88ace1b8844018
karin <kr9277@bs.amalnet.k12.il>
2023-06-17 21:06:22 +0300

    fg

--- app/src/main/java/com/example/proj1/MainActivity.java
+++ app/src/main/java/com/example/proj1/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.proj1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText et1, et2, et3;
-
+    Button btn;
+    String type="";
+    double first;
+    double d = 0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +22,29 @@ public class MainActivity extends AppCompatActivity {
         et1 = findViewById(R.id.et1);
         et2 = findViewById(R.id.et2);
         et3 = findViewById(R.id.et3);
+        btn = findViewById(R.id.btn);
+        type = et1.getText().toString();
+        first = Double.parseDouble(et2.getText().toString());
+        d = Double.parseDouble(et2.getText().toString());
+
+    }
+    public void next(View view) {
+        if(type!="invoicing" && type!="engineering"){
+            Toast.makeText(this, "you need to choose between invoicing or engineering", Toast.LENGTH_SHORT).show();
+            type = et1.getText().toString();
+        }
+        if(first<-1000000.0 || first>1000000.0){
+            Toast.makeText(this, "you need to choose a number between -1000000 to 1000000", Toast.LENGTH_SHORT).show();
+            first = Double.parseDouble(et2.getText().toString());
+        }
+        if(first<-1000000.0 || first>1000000.0){
+            Toast.makeText(this, "you need to choose a number between -1000000 to 1000000", Toast.LENGTH_SHORT).show();
+            d = Double.parseDouble(et3.getText().toString());
+        }
+        Intent si = new Intent(MainActivity.this, SecondActivity.class);
+        si.putExtra("Type", type);
+        si.putExtra("First", first);
+        si.putExtra("delta", d);
+        startActivity(si);
     }
 }
--- app/src/main/java/com/example/proj1/SecondActivity.java
+++ app/src/main/java/com/example/proj1/SecondActivity.java
@@ -2,14 +2,30 @@ package com.example.proj1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    TextView tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8;
+    ListView lv;
+    Intent gi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+        tv4 = findViewById(R.id.tv4);
+        tv5 = findViewById(R.id.tv5);
+        tv6 = findViewById(R.id.tv6);
+        tv7 = findViewById(R.id.tv7);
+        tv8 = findViewById(R.id.tv8);
+        lv = findViewById(R.id.lv);
+        gi = getIntent();
+        S
     }
 }
